422cfba75997467ef25f2ad21b828c075e25806f
Pedro Henrique Bergental <phbergental@brazpine.com>
2023-04-23 20:57:50 -0300

    chore: routes

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,7 +1,22 @@
+import { MealStorageDTO } from '@storage/meal/MealStorageDTO';
+
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined;
+      statistics: undefined;
+      meal: {
+        mealId?: string,
+        backTo?: 'mealDetail' | 'home'
+      };
+      mealDetail: {
+        mealId: string
+      };
+      feedback: {
+        mealId: string,
+        onTheDiet: boolean,
+        backTo?: 'mealDetail' | 'home'
+      };
     }
   }
 }
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,6 +1,11 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Home } from '@screens/Home'
+import { Home } from '@screens/Home';
+import { Statistics } from '@screens/Statistics';
+import { Meal } from '@screens/Meal';
+import { MealDetail } from '@screens/MealDetail';
+import { Feedback } from '@screens/Feedback';
+
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -14,6 +19,26 @@ export function AppRoutes() {
       <Screen 
         name="home"
         component={Home}
+      />
+
+      <Screen 
+        name="statistics"
+        component={Statistics}
+      />
+
+      <Screen 
+        name="meal"
+        component={Meal}
+      />
+
+      <Screen 
+        name="mealDetail"
+        component={MealDetail}
+      />
+
+      <Screen 
+        name="feedback"
+        component={Feedback}
       />
     </Navigator>
   )
