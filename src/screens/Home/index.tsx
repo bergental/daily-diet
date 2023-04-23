bb783a8b58f8fa87df8df89c037039957c8cb312
Pedro Henrique Bergental <phbergental@brazpine.com>
2023-04-23 20:55:13 -0300

    feat: home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,132 @@
-import { Container, Text } from './styles';
+import { useState, useCallback } from 'react';
+import { SectionList, Alert } from 'react-native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+
+import { ButtonStatistics } from '@components/ButtonStatistics';
+import { Button } from '@components/Button';
+import { MealCard } from '@components/MealCard';
+import { parseHourMinute } from '@utils/DateParse';
+
+import { 
+  Container, 
+  ContainerHeader, 
+  Logo, 
+  ProfilePicture, 
+  ContainerMeals, 
+  Title,
+  MealListGroup
+} from './styles';
+
+import logoImg from '@assets/logo.png';
+import profileImg from '@assets/profile.png';
+import { ListEmpty } from '@components/ListEmpty';
+import { MealListDTO, MealStorageDTO } from '@storage/meal/MealStorageDTO';
+import { mealsGetAllToLList } from '@storage/meal/mealsGetAllToList';
+import { Loading } from '@components/Loading';
+import { StatisticsStorageDTO } from '@storage/statistics/StatisticsStorageDTO';
+import { statisticsGet } from '@storage/statistics/statisticsGet';
+import { MEAL_COLLECTION } from '@storage/storageConfig';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [meals, setMeals] = useState<MealListDTO[]>([]);
+  const [statistics, setStatistics] = useState<StatisticsStorageDTO>();
+
+  async function fetchMeals() {
+    try {
+      // await AsyncStorage.removeItem(MEAL_COLLECTION);
+      setIsLoading(true);
+      const data = await mealsGetAllToLList();
+      setMeals(data);
+    } catch (error) {
+      Alert.alert('Refeições', 'Não foi possível carregar as refeições.');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function fetchStatistics() {
+    try {
+      const data = await statisticsGet();
+      setStatistics(data);
+    } catch (error) {
+      Alert.alert('Estatísticas', 'Não foi possível carregar as estatísticas das refeições.');
+    }
+  }
+
+  const navigation = useNavigation();
+
+  function handleStatistics() {
+    navigation.navigate('statistics');
+  }
+  
+  function handleNewMeal() {
+    navigation.navigate('meal', {});
+  }
+  
+  function handleMealDetail(meal: MealStorageDTO) {
+    navigation.navigate('mealDetail', { mealId: meal.id! });
+  }
+  
+  useFocusEffect(useCallback(() => { 
+    fetchMeals();
+    fetchStatistics();
+  }, []));
+
   return (
     <Container>
-      <Text>
-        Home
-      </Text>
+      <ContainerHeader>
+        <Logo source={logoImg} />
+        <ProfilePicture 
+          source={profileImg} 
+          
+        />
+      </ContainerHeader>
+      <ButtonStatistics 
+        percent={statistics ? statistics.percentOnTheDiet : 0}
+        subtitle="das refeições dentro da dieta"
+        onPress={handleStatistics}
+      />
+      <ContainerMeals>
+        <Title>
+          Refeições
+        </Title>
+        <Button
+          icon='Add'
+          title='Nova refeição'
+          style={{
+            marginBottom: 12
+          }}
+          onPress={handleNewMeal}
+        />
+        {
+        isLoading ?
+          <Loading /> :
+          <SectionList 
+            sections={meals}
+            keyExtractor={(item, index) => `${item.date}_${index}`}
+            contentContainerStyle={meals.length == 0 && { flex: 1 }}
+            renderItem={({item}) => (
+              <MealCard 
+                time={parseHourMinute(new Date(item.date))}
+                title={item.name}
+                status={item.onTheDiet ? 'Y' : 'N'}
+                onPress={() => handleMealDetail(item)}
+              />
+            )}
+            renderSectionHeader={({section: {group}}) => (
+              <MealListGroup>{group}</MealListGroup>
+            )}
+            ListEmptyComponent={() => (
+              <ListEmpty 
+                message="Que tal cadastrar a primeira refeição?"
+              />
+            )}
+            showsVerticalScrollIndicator={false}
+          />
+          }
+      </ContainerMeals>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,15 +4,51 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_100};
-  padding: 24px;  
-  
-  justify-content: center;
-  align-items: center;
+  padding: 24px;
+  padding-bottom: 0px;
 `;
 
-export const Text = styled.Text`
+export const ContainerHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 32px;
+`;
+
+export const Logo = styled.Image`
+  width: 82px;
+  height: 37px;
+`;
+
+export const ProfilePicture = styled.Image`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  border-width: 2px;
+  border-color: ${({ theme }) => theme.COLORS.GRAY_600 };
+`;
+
+export const ContainerMeals = styled.View`
+  flex: 1;
+  margin-top: 40px;
+`;
+
+export const Title = styled.Text`
   ${({ theme }) => css`
-    font-size: ${theme.FONT_SIZE.XXL}px;
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.GRAY_700};
     font-family: ${theme.FONT_FAMILY.REGULAR};
   `}
-`;
+  margin-bottom: 8px;
+`
+
+export const MealListGroup = styled.Text`
+  align-self: flex-start;
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.LG}px;
+    color: ${theme.COLORS.GRAY_700};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
+  margin-top: 20px;
+  margin-bottom: 8px;
+`
